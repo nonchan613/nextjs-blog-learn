9ccfdee7387d27982d369ea5f62c97710435e959
Nozomi <nozomi.h0613@gmail.com>
2023-11-07 10:51:58 +0900

    feat: Create /posts/first-posts

---
+++ src/pages/posts/first-posts.tsx
@@ -0,0 +1,13 @@
+// src/pages/posts/first-post.tsx
+const FirstPost = () => {
+	return (
+    <h1>First Post</h1>
+	)
+  };
+
+  export default FirstPost;
+
+// function Firstpost(){
+
+// }
+// ↑これと同じ
